327a93baaca69f981e1f9b78d24e3eb8b3db9e19
djo kangoma <djokangoma@gmail.com>
2024-01-31 17:12:18 +0100

    mise en main du tailwing avec son utilisation

--- src/App.jsx
+++ src/App.jsx
@@ -24,77 +24,93 @@ export default function App() {
   //   ('$formData.gender==="femme"? "enregistrée":"enregistré"');
   // }
   return (
-    <div>
+    <div className="flex flex-col p-5  bg-purple-600 w-80 h-90 rounded-b-lg m-10">
       <div>
-        <h1>contact form</h1>
+        <h1 className="text-2xl font-semibold text-center text-white ">
+          contact form
+        </h1>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            {...register("name", {
-              required: "ce champ est obligatoire",
-            })}
-          />
-          {errors.name && (
-            <span style={{ color: "red" }}>{errors.name.message}</span>
-          )}
-        </div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label className="text-white">Name</label>
+            <input
+              className="bg-pink-50 rounded-full mb-4 ml-4 mt-4"
+              type="text"
+              name="name"
+              {...register("name", {
+                required: "ce champ est obligatoire",
+              })}
+            />
+            {errors.name && (
+              <span style={{ color: "red" }}>{errors.name.message}</span>
+            )}
+          </div>
 
-        <div>
-          <label>Telephone</label>
-          <input
-            type="text"
-            name="phone"
-            {...register("phone", {
-              required: "ce champ est obligatoire",
-              pattern: {
-                value: /^[0-9]{10}$/i,
-                message: "ce champ n'est pas au bon format",
-              },
-            })}
-          />
-          {errors.phone && (
-            <span style={{ color: "red" }}>{errors.phone.message}</span>
-          )}
-        </div>
+          <div>
+            <label className="text-white">Telephone</label>
+            <input
+              className="bg-pink-50 rounded-full mb-4 ml-4"
+              type="text"
+              name="phone"
+              {...register("phone", {
+                required: "ce champ est obligatoire",
+                pattern: {
+                  value: /^[0-9]{10}$/i,
+                  message: "ce champ n'est pas au bon format",
+                },
+              })}
+            />
+            {errors.phone && (
+              <span style={{ color: "red" }}>{errors.phone.message}</span>
+            )}
+          </div>
 
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            {...register("email", { required: "ce champ est obligatoire" })}
-          />
-          {errors.phone && (
-            <span style={{ color: "red" }}>{errors.email.message}</span>
-          )}
-        </div>
+          <div>
+            <label className="text-white">Email</label>
+            <input
+              className="bg-pink-50 rounded-full mb-4 ml-4"
+              type="email"
+              name="email"
+              {...register("email", { required: "ce champ est obligatoire" })}
+            />
+            {errors.phone && (
+              <span style={{ color: "red" }}>{errors.email.message}</span>
+            )}
+          </div>
 
-        <div>
-          <label>Age</label>
-          <input
-            type="number"
-            name="age"
-            {...register("age", { required: "ce champ est obligatoire" })}
-          />
-          {errors.phone && (
-            <span style={{ color: "red" }}>{errors.email.message}</span>
-          )}
-        </div>
+          <div>
+            <label className="text-white">Age</label>
+            <input
+              className="bg-pink-50 rounded-full mb-4 ml-4"
+              type="number"
+              name="age"
+              {...register("age", { required: "ce champ est obligatoire" })}
+            />
+            {errors.phone && (
+              <span style={{ color: "red" }}>{errors.email.message}</span>
+            )}
+          </div>
 
-        <div>
-          <label>Genre</label>
-          <select {...register("name")}>
-            <option value="Homme">Homme</option>
-            <option value="Femme">Femme</option>
-            <option value="Autre">Autre</option>
-          </select>
-        </div>
-        <button type="submit">Enregistrer</button>
-      </form>
+          <div>
+            <label className="text-white">Genre</label>
+            <select
+              {...register("name")}
+              className="bg-pink-50 rounded-full mb-4 ml-4"
+            >
+              <option value="Homme">Homme</option>
+              <option value="Femme">Femme</option>
+              <option value="Autre">Autre</option>
+            </select>
+          </div>
+          <button
+            className="bg-gradient-to-r from-gray-400 to-white-500 hover:from-pink-500 hover:to-yellow-500 rounded-lg p-2 text-white"
+            type="submit"
+          >
+            Enregistrer
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
